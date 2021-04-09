d2be5ca0574c09b8ed9f28d4d32fd4691c97d824
Pavel Salva <pavel.salva@4net.tv>
2021-04-09 18:58:06 +0200

    +

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 A library for adding [Sign In With Apple](https://developer.apple.com/sign-in-with-apple/) to Android apps.
 
-[![CircleCI](https://circleci.com/gh/willowtreeapps/sign-in-with-apple-button-android.svg?style=svg&circle-token=94aaaafd543585e19434a36498601ec291d29e62)](https://circleci.com/gh/willowtreeapps/sign-in-with-apple-button-android) [![License MIT](https://img.shields.io/badge/License-MIT-blue.svg?style=flat)]() [![Public Yes](https://img.shields.io/badge/Public-yes-green.svg?style=flat)]()
-
 ## How it works
 
 This library includes a `SignInWithAppleButton` class. You can style the button according to Apple's [Human Interface Guidelines](https://developer.apple.com/design/human-interface-guidelines/sign-in-with-apple/overview/).
--- signIn-with-apple/src/main/java/com/willowtreeapps/signinwithapplebutton/view/SignInWithAppleButton.kt
+++ signIn-with-apple/src/main/java/com/willowtreeapps/signinwithapplebutton/view/SignInWithAppleButton.kt
@@ -11,80 +11,16 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.button.MaterialButton
 import com.willowtreeapps.signinwithapplebutton.*
 
 class SignInWithAppleButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : MaterialButton (context, attrs, defStyleAttr)  {
 
     internal companion object {
         const val SIGN_IN_WITH_APPLE_LOG_TAG = "SIGN_IN_WITH_APPLE"
-    }
-
-    init {
-        LayoutInflater.from(context).inflate(R.layout.sign_in_with_apple_button, this, true)
-    }
-
-    private val imageView: ImageView = findViewById(R.id.imageView)
-    private val textView: TextView = findViewById(R.id.textView)
-
-    init {
-        val attributes =
-            context.theme.obtainStyledAttributes(attrs, R.styleable.SignInWithAppleButton, 0, R.style.SignInWithAppleButton)
-
-        // Style
-        val background = attributes.getDrawable(R.styleable.SignInWithAppleButton_android_background)
-        val icon = attributes.getResourceId(R.styleable.SignInWithAppleButton_android_drawableLeft, -1)
-        val iconSize = attributes.getDimensionPixelSize(R.styleable.SignInWithAppleButton_sign_in_with_apple_button_iconSize, -1)
-        val textColor = attributes.getColorStateList(R.styleable.SignInWithAppleButton_android_textColor)
-        val textSize = attributes.getDimensionPixelSize(R.styleable.SignInWithAppleButton_android_textSize, -1)
-        val textStyle = attributes.getInt(R.styleable.SignInWithAppleButton_android_textStyle, 0)
-        val fontFamily = attributes.getString(R.styleable.SignInWithAppleButton_android_fontFamily)
-
-        // Text type
-        val text = attributes.getInt(
-            R.styleable.SignInWithAppleButton_sign_in_with_apple_button_textType,
-            SignInTextType.SIGN_IN.ordinal
-        )
-
-        // Corner radius
-        val cornerRadius = attributes.getDimension(
-            R.styleable.SignInWithAppleButton_sign_in_with_apple_button_cornerRadius,
-            resources.getDimension(R.dimen.sign_in_with_apple_button_cornerRadius_default)
-        )
-
-        attributes.recycle()
-
-        this.background = background?.mutate()
-        (background as? GradientDrawable)?.cornerRadius = cornerRadius
-
-        if (icon != -1) {
-            imageView.setImageResource(icon)
-
-            if (iconSize != -1) {
-                imageView.layoutParams.width = iconSize
-                imageView.layoutParams.height = iconSize
-            }
-        } else {
-            imageView.visibility = View.GONE
-        }
-
-        textView.setTextColor(textColor)
-
-        if (textSize != -1) {
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize.toFloat())
-        }
-
-        val typeface = if (fontFamily == null) {
-            Typeface.create(textView.typeface, textStyle)
-        } else {
-            Typeface.create(fontFamily, textStyle)
-        }
-
-        textView.typeface = typeface
-
-        textView.text = resources.getString(SignInTextType.values()[text].text)
     }
 
     fun setUpSignInWithAppleOnClick(
